0afd2ef5a912ffbf8049fdf02e6c394d1174022a
furkandurkac4601 <furkandurkac4601@gmail.com>
2025-11-06 17:50:21 +0300

    Add triangle area calculation using Heron's formula

--- GeometrikHesap.java
+++ GeometrikHesap.java
@@ -1,1 +1,37 @@
+package geometri;
+
+import java.text.DecimalFormat;
+
+public class UcgenAlani {
+
+    public static void main(String[] args) {
+        // 🔹 Üçgenin kenar uzunluklarını tanımla
+        double kenarA = 5.0;
+        double kenarB = 7.0;
+        double kenarC = 9.0;
+
+        // 🔹 Yarı çevreyi hesapla (Heron formülü için gerekli)
+        double yariCevre = (kenarA + kenarB + kenarC) / 2;
+
+        // 🔹 Heron formülüyle alanı hesapla
+        double alan = Math.sqrt(
+                yariCevre *
+                (yariCevre - kenarA) *
+                (yariCevre - kenarB) *
+                (yariCevre - kenarC)
+        );
+
+        // 🔹 Sayısal biçimlendirme (2 ondalık basamak)
+        DecimalFormat df = new DecimalFormat("#.##");
+
+        // 🔹 Sonuçları ekrana yazdır
+        System.out.println("=== ÜÇGEN ALANI HESAPLAMA ===");
+        System.out.println("Kenar A: " + kenarA);
+        System.out.println("Kenar B: " + kenarB);
+        System.out.println("Kenar C: " + kenarC);
+        System.out.println("-------------------------------");
+        System.out.println("Yarı Çevre: " + df.format(yariCevre));
+        System.out.println("Üçgenin Alanı: " + df.format(alan));
+    }
+}
 
